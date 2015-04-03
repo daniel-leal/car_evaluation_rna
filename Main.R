8628c3468716746a700d6e21af3955504bcf705c
Daniel Leal <danielleal94@gmail.com>
2015-04-03 12:03:21 -0300

    finish

--- Main.R
+++ Main.R
@@ -1,3 +1,8 @@
+library("nnet")
+library("lattice")
+library("ggplot2")
+library("caret")
+
 # Load Data
 data <- read.table("../car_evaluation/car.csv", header = T, sep = ",")
 
@@ -25,3 +30,28 @@ head(preds2)
 
 # Check the accuracy of the model
 postResample(dataTest$result,preds2)
+
+totalAccuracy <- c()
+cv <- 10
+cvDivider <- floor(nrow(data) / (cv+1))
+
+for (cv in seq(1:cv)) {
+  # assign chunk to data test
+  dataTestIndex <- c((cv * cvDivider):(cv * cvDivider + cvDivider))
+  dataTest <- data[dataTestIndex,]
+  # everything else to train
+  dataTrain <- data[-dataTestIndex,]
+  
+  resultModel <- multinom(result~., data=dataTrain, maxit=50, trace=T) 
+  
+  pred <- predict(resultModel, newdata=dataTest, type="class")
+  
+  #  classification error
+  cv_ac <- postResample(dataTest$result, pred)[[1]]
+  print(paste('Current Accuracy:',cv_ac,'for CV:',cv))
+  totalAccuracy <- c(totalAccuracy, cv_ac)
+}
+
+plot(totalAccuracy, xlab="Predict")
+print(paste('Mean of total accuracy:', mean(totalAccuracy)))
+
